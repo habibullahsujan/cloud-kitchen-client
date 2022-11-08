476a971bc5085a09394bb91277e17959094bce23
habibullahsujan <habibsujan007@gmail.com>
2022-11-08 10:34:18 +0600

    home section complete

---
+++ src/Components/Home/FoodBlog.js
@@ -0,0 +1,150 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const FoodBlog = () => {
+  return (
+    <div>
+      <section className="py-6 sm:py-12 bg-white text-black">
+        <div className="container p-6 mx-auto space-y-8">
+          <div className="space-y-2 text-center">
+            <h2 className="text-5xl font-bold">Latest From Our Blog</h2>
+            <p className="font-serif text-sm dark:text-gray-400">
+              Qualisque erroribus usu at, duo te agam soluta mucius.
+            </p>
+          </div>
+          <div className="grid grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-2 lg:grid-cols-4">
+            <article className="flex flex-col dark:bg-gray-900">
+              <Link
+               
+              >
+                <img
+                  alt=""
+                  className="object-cover w-full h-52 dark:bg-gray-500"
+                  src="https://images.pexels.com/photos/6937457/pexels-photo-6937457.jpeg?auto=compress&cs=tinysrgb&w=600"
+                />
+              </Link>
+              <div className="flex flex-col flex-1 p-6">
+                <Link
+             
+                ></Link>
+                <Link
+                  className="text-xs tracking-wider uppercase hover:underline dark:text-violet-400"
+                >
+                  Convenire
+                </Link>
+                <h3 className="flex-1 py-2 text-lg font-semibold leading-snug">
+                Cooking Perfect Fried Rice in minutes
+                </h3>
+                <div className="flex flex-wrap justify-between pt-3 space-x-2 text-xs dark:text-gray-400">
+                  <span>June 1, 2020</span>
+                  <span>2.1K views</span>
+                </div>
+              </div>
+            </article>
+            <article className="flex flex-col dark:bg-gray-900">
+              <Link
+                aria-label="Te nulla oportere reprimique his dolorum"
+              >
+                <img
+                  alt=""
+                  className="object-cover w-full h-52 dark:bg-gray-500"
+                  src="https://images.pexels.com/photos/824635/pexels-photo-824635.jpeg?auto=compress&cs=tinysrgb&w=600"
+                />
+              </Link>
+              <div className="flex flex-col flex-1 p-6">
+                <Link
+                 
+                  href="#"
+                  
+                ></Link>
+                <Link
+                
+                  className="text-xs tracking-wider uppercase hover:underline dark:text-violet-400"
+                >
+                  Convenire
+                </Link>
+                <h3 className="flex-1 py-2 text-lg font-semibold leading-snug">
+                  TSecret of making Heart Shaped eggs.
+                </h3>
+                <div className="flex flex-wrap justify-between pt-3 space-x-2 text-xs dark:text-gray-400">
+                  <span>June 2, 2020</span>
+                  <span>2.2K views</span>
+                </div>
+              </div>
+            </article>
+            <article className="flex flex-col dark:bg-gray-900">
+              <Link
+                rel="noopener noreferrer"
+                href="#"
+                aria-label="Te nulla oportere reprimique his dolorum"
+              >
+                <img
+                  alt=""
+                  className="object-cover w-full h-52 dark:bg-gray-500"
+                  src="https://images.pexels.com/photos/8477228/pexels-photo-8477228.jpeg?auto=compress&cs=tinysrgb&w=600"
+                />
+              </Link>
+              <div className="flex flex-col flex-1 p-6">
+                <Link
+                 
+                  href="#"
+                  
+                ></Link>
+                <Link
+                  
+                  href="#"
+                  className="text-xs tracking-wider uppercase hover:underline dark:text-violet-400"
+                >
+                  Convenire
+                </Link>
+                <h3 className="flex-1 py-2 text-lg font-semibold leading-snug">
+                How to check steak if it is tender or not.
+                </h3>
+                <div className="flex flex-wrap justify-between pt-3 space-x-2 text-xs dark:text-gray-400">
+                  <span>June 3, 2020</span>
+                  <span>2.3K views</span>
+                </div>
+              </div>
+            </article>
+            <article className="flex flex-col dark:bg-gray-900">
+              <Link
+                rel="noopener noreferrer"
+                href="#"
+                aria-label="Te nulla oportere reprimique his dolorum"
+              >
+                <img
+                  alt=""
+                  className="object-cover w-full h-52 dark:bg-gray-500"
+                  src="https://images.pexels.com/photos/1603901/pexels-photo-1603901.jpeg?auto=compress&cs=tinysrgb&w=600"
+                />
+              </Link>
+              <div className="flex flex-col flex-1 p-6">
+                <Link
+                  rel="noopener noreferrer"
+                  href="#"
+                  aria-label="Te nulla oportere reprimique his dolorum"
+                ></Link>
+                <Link
+                  rel="noopener noreferrer"
+                  href="#"
+                  className="text-xs tracking-wider uppercase hover:underline dark:text-violet-400"
+                >
+                  Convenire
+                </Link>
+                <h3 className="flex-1 py-2 text-lg font-semibold leading-snug">
+                Seaseme and black seed Flavored Bun Rocks.
+                </h3>
+                <div className="flex flex-wrap justify-between pt-3 space-x-2 text-xs dark:text-gray-400">
+                  <span>June 4, 2020</span>
+                  <span>2.4K views</span>
+                </div>
+              </div>
+            </article>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default FoodBlog;
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,7 +2,9 @@ import React from "react";
 import { Link, useLoaderData } from "react-router-dom";
 import Services from "./Services";
 import Slider from "./Slider";
-import '../BasicStyles.css'
+import "../BasicStyles.css";
+import About from "./About";
+import FoodBlog from "./FoodBlog";
 
 const Home = () => {
   const services = useLoaderData();
@@ -11,23 +13,25 @@ const Home = () => {
     <div>
       <div className="my-10 lg:w-[85%] mx-auto relative">
         <div className="slider-header">
-        <Slider />
+          <Slider />
         </div>
-  
-        <div className="absolute top-[40%] left-24 z-50 text-left ">
+
+        <div className="absolute top-[35%] left-24 z-50 text-left ">
           <div className="lg:w-[70%]">
-          <span>Wide option of choice.</span>
-          <h1 className="font-bold text-6xl">Delicious Recipes </h1>
-          <p>
-            Inappropriate behavior is often laughed off as "boys will be boys"
-            women face higher conduct standards especially in the workplace.
-            That's why it's crucial that, as women.{" "}
-          </p>
+            <span>Wide option of choice.</span>
+            <h1 className="font-bold text-6xl my-4">Delicious Recipes </h1>
+            <p>
+              Inappropriate behavior is often laughed off as "boys will be boys"
+              women face higher conduct standards especially in the workplace.
+              That's why it's crucial that, as women.{" "}
+            </p>
+            <button className="border border-sky-600 px-4 py-2 my-4 font-semibold">
+              Check Our Menu
+            </button>
           </div>
-        
         </div>
       </div>
-      <div>
+      <div className="my-20">
         <h1 className="font-bold text-4xl">Services</h1>
       </div>
       <div className="grid lg:grid-cols-3 grid-cols-1 mx-auto lg:w-[85%] gap-2">
@@ -39,6 +43,12 @@ const Home = () => {
         <button className="border border-x-blue-300 font-semibold px-4 py-2">
           <Link to={"/allServices"}>See More</Link>
         </button>
+      </div>
+      <div>
+        <About/>
+      </div>
+      <div>
+        <FoodBlog/>
       </div>
     </div>
   );
